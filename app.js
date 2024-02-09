16b490345fa14f79dbcb3e1bc396c26b5d346c9b
Yerinson Muentes <yerindd@gmail.com>
2024-02-08 23:34:40 -0300

    pg agregado

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+import {pg} from 'pg'
+
 const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
+
+const pool = new pg.Pool({
+    connectionString: process.env.DATA_URL
+})
 
 dotenv.config();
 
@@ -12,9 +18,12 @@ app.use(bodyParser.json()) // for parsing application/json
 
 
 
+app.get('/ping'), async (req,res)=>{
+    const result = await pool.query('SELECT NOW()')
+    return res.JSON(result.rows[0])
+}
 
-
-app.use("/", registros);
+app.use("/datos ", registros);
 
 // const port = process.env.PORT || 3000;
 // app.listen(port, () => console.log(`escuchando en puerto ${port}...`));
