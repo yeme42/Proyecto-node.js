8753cb3caaf17107ca3a09e387f2db9788918be0
Yerinson Muentes <yerindd@gmail.com>
2024-02-19 21:55:56 -0300

    permisos de cors activos

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const dotenv = require('dotenv');
+const cors = require('cors')
 const express = require('express');
 const app = express();
 
@@ -6,6 +7,15 @@ dotenv.config();
 
 const registros = require('./consl/registros.js');
 const bodyParser = require('body-parser')
+
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     next();
+//   });
+app.use(cors())
+    
+
 
 app.use(bodyParser.json()) // for parsing application/json
 
@@ -16,5 +26,5 @@ app.use(bodyParser.json()) // for parsing application/json
 
 app.use("/datos", registros);
 
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => console.log(`escuchando en puerto ${port}...`));
+const port = process.env.port || 3000;
+app.listen(port, () => console.log(`escuchando en puerto ${port}...`));
--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const knex = require("knex")({
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      database: process.env.DB_NAME
     },
     pool: {min: 0, max: 10}
 });
